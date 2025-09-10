6ee12795ec1438bf23abcee032990299fb0554d8
randomRedMage <christesen.scott22@gmail.com>
2025-09-10 20:38:36 +1200

    Tripeak (#15)
    * Tripeaks
    
    Base tripeaks implementation
    
    * Fixed issue with tripeak card placement
    
    * Fixed response area when clicking cards

--- src/solitaire/scenes/menu.py
+++ src/solitaire/scenes/menu.py
@@ -11,6 +11,7 @@ class MainMenuScene(C.Scene):
         self.b_klon = C.Button("Play Klondike", cx, y, center=True); y += 60
         self.b_free = C.Button("Play FreeCell", cx, y, center=True); y += 60
         self.b_pyr  = C.Button("Play Pyramid",  cx, y, center=True); y += 60
+        self.b_tri  = C.Button("Play TriPeaks", cx, y, center=True); y += 60
         self.b_settings = C.Button("Settings", cx, y, center=True); y += 60
         self.b_quit = C.Button("Quit", cx, y, center=True)
 
@@ -27,6 +28,9 @@ class MainMenuScene(C.Scene):
             elif self.b_pyr.hovered((mx,my)):
                 from solitaire.modes.pyramid import PyramidOptionsScene
                 self.next_scene = PyramidOptionsScene(self.app)
+            elif self.b_tri.hovered((mx,my)):
+                from solitaire.modes.tripeaks import TriPeaksOptionsScene
+                self.next_scene = TriPeaksOptionsScene(self.app)
             elif self.b_settings.hovered((mx,my)):
                 from solitaire.scenes.settings import SettingsScene
                 self.next_scene = SettingsScene(self.app)
@@ -41,5 +45,5 @@ class MainMenuScene(C.Scene):
         title = C.FONT_TITLE.render("Solitaire Suite", True, C.WHITE)
         screen.blit(title, (C.SCREEN_W//2 - title.get_width()//2, 120))
         mp = pygame.mouse.get_pos()
-        for b in [self.b_klon, self.b_free, self.b_pyr, self.b_settings, self.b_quit]:
+        for b in [self.b_klon, self.b_free, self.b_pyr, self.b_tri, self.b_settings, self.b_quit]:
             b.draw(screen, hover=b.hovered(mp))
